70b5d5f9c57b773ed6becbd9deb4ec57b64fbd12
Moshe Wainner <Moshewiner13@gmail.com>
2020-11-07 22:12:44 +0200

    added a lot of pages

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  constructor() {
+  }
+
+}
---
+++ src/app/pages/beauty-zone/beauty-zone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beauty-zone',
+  templateUrl: './beauty-zone.component.html',
+  styleUrls: ['./beauty-zone.component.scss']
+})
+export class BeautyZoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
