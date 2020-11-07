d5fe599cf1dedc84995e0d3f2aa17ab518f255fc
Moshe Wainner <Moshewiner13@gmail.com>
2020-11-07 22:12:15 +0200

    added a lot of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {BeautyZoneComponent} from './pages/beauty-zone/beauty-zone.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'beautyzone', component: BeautyZoneComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductComponent } from './pages/home/product/product.component';
+
+import { NgxParallaxModule } from '@yoozly/ngx-parallax';
+import { AboutComponent } from './pages/home/about/about.component';
+import { BeautyZoneComponent } from './pages/beauty-zone/beauty-zone.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    AboutComponent,
+    BeautyZoneComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxParallaxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
