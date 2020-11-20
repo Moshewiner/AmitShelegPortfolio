16c0232ddbb35702b59c5313154267b97f8404b9
Moshe Wainner <Moshewiner13@gmail.com>
2020-11-20 17:12:37 +0200

    Added pages and fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,29 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent {
-
-  constructor() {
+  styleUrls: ['./header.component.scss'],
+  host: {
+    class: 'navbar'
   }
+})
+export class HeaderComponent implements OnInit {
+  
+  constructor() {
 
+  }
+  ngOnInit(): void {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+  //      console.log({ nav: document.getElementsByClassName("navbar")[0] });
+        document.getElementsByClassName("navbar")[0].style.top = "0";
+      } else {
+        document.getElementsByClassName("navbar")[0].style.top = "-70px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
 }
