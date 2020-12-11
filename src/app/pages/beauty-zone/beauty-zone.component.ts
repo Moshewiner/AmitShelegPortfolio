ceae38837cb2363b5164d2c42dfe4ccef90e3ba1
Moshe Wainner <Moshewiner13@gmail.com>
2020-12-11 22:07:08 +0200

    introduction page done

--- src/app/pages/beauty-zone/beauty-zone.component.ts
+++ src/app/pages/beauty-zone/beauty-zone.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeautyZoneComponent implements OnInit {
 
+
+    public texts: {[key: string]: string} = {
+      introduction: `How many times have you run into a stunning makeup look and wished to know how to do it on your own? Or maybe find out what mascara did she use to get those outstanding lashes?
+
+      Have you spent too much money and hours trying to get the desired look and finally got exhausted and unsatisfied from the result?
+      
+      Meet BeautyZone.`
+    };
+
   constructor() { }
 
   ngOnInit(): void {
