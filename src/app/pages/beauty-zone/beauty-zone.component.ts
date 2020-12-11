a12ce052afe6f3ae4c2d02dcfa85894a270072f0
Moshe Wainner <Moshewiner13@gmail.com>
2020-12-11 22:34:29 +0200

    user-research page done

--- src/app/pages/beauty-zone/beauty-zone.component.ts
+++ src/app/pages/beauty-zone/beauty-zone.component.ts
@@ -13,7 +13,14 @@ export class BeautyZoneComponent implements OnInit {
 
       Have you spent too much money and hours trying to get the desired look and finally got exhausted and unsatisfied from the result?
       
-      Meet BeautyZone.`
+      Meet BeautyZone.`,
+      userResearch: `The first part of the project was to conduct interviews with people from the beauty world, both content creator
+      and content consumer. It allowed me to understand their needs and desires in order to identify the pain points
+      they encounter.
+      I learned from the interviews that in order to learn and discover the best products and techniques
+      independently, they mainly use free platform such as Instagram, Pinterest and YouTube. If they wish to level up
+      their makeup skills to become professionals, they have to sign up for paid courses.
+      Sadly, It takes a long way to practice makeup or even just to master makeup as a hobby.`,
     };
 
   constructor() { }
