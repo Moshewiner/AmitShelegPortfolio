eb61b7351dade94e6b0b24ba6779c0c2b4ffd308
Moshe Wainner <Moshewiner13@gmail.com>
2020-12-25 18:08:42 +0200

    Added personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AmitSheleg';
+  // private screens: any;
+  // private pos = 0;
+  
+  // private next() {
+  //   this.pos++;
+  //   const e = this.screens[this.pos];
+  //   e.scrollIntoView();
+  // }
+  // private previous() {
+  //   this.pos--;
+  //   const e = this.screens[this.pos];
+  //   e.scrollIntoView();
+  // }
+  // private oldValue: number = 0;
+
+  // constructor(@Inject(DOCUMENT) private _document: HTMLDocument) {}
+  //   ngOnInit() {
+  //   this.screens = this._document.querySelectorAll('.screen');
+  //   console.log({screens: this.screens});
+  //   window.addEventListener('scroll', (e) => {
+  //     const newValue = window.pageYOffset;
+ 
+  //     if (this.oldValue - newValue < 0) {
+  //       this.next();
+  //     } else if (this.oldValue - newValue > 0) {
+  //       this.previous();
+  //     }
+  //     this.oldValue = newValue;
+  //   });
+
+  // }
 }
