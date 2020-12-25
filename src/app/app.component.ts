fd5d761bdf0c9235b18b549780edb396a80354b1
Daniel Livshim <Livshind12@gmail.com>
2020-12-26 00:10:54 +0200

    try no do scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,39 @@
 import { Component, ElementRef, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common'
-
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AmitSheleg';
-  // private screens: any;
-  // private pos = 0;
-  
-  // private next() {
-  //   this.pos++;
-  //   const e = this.screens[this.pos];
-  //   e.scrollIntoView();
-  // }
-  // private previous() {
-  //   this.pos--;
-  //   const e = this.screens[this.pos];
-  //   e.scrollIntoView();
-  // }
-  // private oldValue: number = 0;
+  private screens: any;
+  private isScrolling = false;
 
-  // constructor(@Inject(DOCUMENT) private _document: HTMLDocument) {}
-  //   ngOnInit() {
-  //   this.screens = this._document.querySelectorAll('.screen');
-  //   console.log({screens: this.screens});
-  //   window.addEventListener('scroll', (e) => {
-  //     const newValue = window.pageYOffset;
- 
-  //     if (this.oldValue - newValue < 0) {
-  //       this.next();
-  //     } else if (this.oldValue - newValue > 0) {
-  //       this.previous();
-  //     }
-  //     this.oldValue = newValue;
-  //   });
+  constructor(@Inject(DOCUMENT) private _document: HTMLDocument) {}
 
-  // }
+  ngOnInit() {
+    setTimeout(() => {}, 400);
+    window.addEventListener('wheel', (e) => {
+      if (!this.isScrolling) {
+        this.isScrolling = true;
+        this.screens = this._document.querySelectorAll('.screen');
+        const howFarFromTop = $(document).scrollTop(); //how far from the top have we scrolled?
+        const currentWindowHeight = $(window).height(); //current window height for responsiveness
+        const delta = e.deltaY; // just to know if it is scroll wheel up or down
+        //find out what is our offset from the top so we can now how far do we have to scroll to  the next / previous element
+        var currentSlide = Math.floor(howFarFromTop / currentWindowHeight); //approximate which slide is on screen at the moment
+
+        if (delta > 0) {
+          //
+        } else {
+          //
+        }
+        setTimeout(() => {
+          this.isScrolling = false;
+        }, 400);
+      }
+    });
+  }
 }
