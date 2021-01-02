91b3164f3fd6eeacf4394c86770f748465e2cd73
Moshe Wainner <Moshewiner13@gmail.com>
2021-01-02 16:10:54 +0200

    Added favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common'
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -9,35 +8,4 @@ import { DOCUMENT } from '@angular/common'
 })
 export class AppComponent {
   title = 'AmitSheleg';
-  // private screens: any;
-  // private pos = 0;
-  
-  // private next() {
-  //   this.pos++;
-  //   const e = this.screens[this.pos];
-  //   e.scrollIntoView();
-  // }
-  // private previous() {
-  //   this.pos--;
-  //   const e = this.screens[this.pos];
-  //   e.scrollIntoView();
-  // }
-  // private oldValue: number = 0;
-
-  // constructor(@Inject(DOCUMENT) private _document: HTMLDocument) {}
-  //   ngOnInit() {
-  //   this.screens = this._document.querySelectorAll('.screen');
-  //   console.log({screens: this.screens});
-  //   window.addEventListener('scroll', (e) => {
-  //     const newValue = window.pageYOffset;
- 
-  //     if (this.oldValue - newValue < 0) {
-  //       this.next();
-  //     } else if (this.oldValue - newValue > 0) {
-  //       this.previous();
-  //     }
-  //     this.oldValue = newValue;
-  //   });
-
-  // }
 }
