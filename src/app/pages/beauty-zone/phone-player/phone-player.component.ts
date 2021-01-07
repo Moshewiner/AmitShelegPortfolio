6a77ff1ec04d8d1643d7c745ff8b71bd61a147aa
moshewa <moshewa@etoro.com>
2021-01-07 22:38:27 +0200

    Added assets

--- src/app/pages/beauty-zone/phone-player/phone-player.component.ts
+++ src/app/pages/beauty-zone/phone-player/phone-player.component.ts
@@ -7,8 +7,9 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhonePlayerComponent implements OnInit {
-  @Input() videoSrc: string = "https://www.youtube.com/embed/A7ry4cx6HfY";
-  
+  // @Input() videoSrc: string = "https://media.giphy.com/media/VRhsYYBw8AE36/giphy.gif";
+
+  @Input() videoSrc: string = "https://store-images.s-microsoft.com/image/apps.44424.13946487343453322.7f94d75c-3c41-4a61-93d9-41aed210dea3.7e33abda-0ccc-42b1-aaab-3832e08bb141?mode=scale&q=90&h=720&w=1280";
   ngOnInit(): void {
   }
 }
