d9258e9b8708f05748535b11220a69b401c29c81
moshewa <moshewa@etoro.com>
2021-01-12 23:29:55 +0200

    Scrolling starting from top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy',  anchorScrolling: 'enabled', useHash: false, })],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy',  anchorScrolling: 'enabled', useHash: false, scrollPositionRestoration: 'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
