c70de4fafbcee437937bc432e2126983c6e572ba
moshewa <moshewa@etoro.com>
2021-08-07 22:20:20 +0300

    beauty zone mobiled

--- src/app/pages/beauty-zone/phone-player/phone-player.component.ts
+++ src/app/pages/beauty-zone/phone-player/phone-player.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PhonePlayerComponent implements OnInit {
   @Input() videoSrc: string = "https://miro.medium.com/max/480/1*mI__FzE0a8M5-6hO_8OPkA.gif";
 
-
+  public svgId: string = (Math.random() + 1).toString(36).substring(7);
 
   ngOnInit(): void {
   }
