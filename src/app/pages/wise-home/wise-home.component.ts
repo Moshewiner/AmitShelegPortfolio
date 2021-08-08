bef52707085dc26094f80c860f725b79d0cc2331
moshewa <moshewa@etoro.com>
2021-08-08 19:25:03 +0300

    wise home

--- src/app/pages/wise-home/wise-home.component.ts
+++ src/app/pages/wise-home/wise-home.component.ts
@@ -8,21 +8,24 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   styleUrls: ['./wise-home.component.scss'],
 })
 export class WiseHomeComponent {
+  constructor(private deviceService: DeviceDetectorService, private router: Router) {
+    this.isNotDesktopDevice = !this.deviceService.isDesktop();
+  }
   public route = '/wisehome';
 
   public texts: { [key: string]: string } = {
-    introduction: "In today's technological and advanced era, we enjoy a multitude of smart devices. Those devices improve the quality of our lives and make them easier, safer, and more comfortable.\n\nOur goal is to enable smart and efficient management for our devices which will provide us controlling tools, and real-time monitoring for everyone, everywhere.",
-    userResearch: "The first step was to go out and speak to users to recognize their pain points. After interviewing a few users, I also read user reviews of known smart home apps.\n\nThe main issue the users pointed at is that there's a specific app for each device's company, which leads to having too many apps to manage all smart devices in their homes.\nThere were also significant complaints regarding the user interface and interaction elements - most dashboard designs were complex and overwhelming for the users.",
-    informationArchitecture: "Based on the insights gained from the research, I defined the architecture of the app. This step helps me to validate my solution before jumping to visual wireframes.",
-    phoneDesign: "The final step was to design user interfaces for mobile and tablet apps. I aimed for a clean design that will focus the user on the main highlights, so I used white-on-white cards and accents them with teal color.\n\n Routines are running automatically, and updates will be trigger push notifications to help the user live with a little fewer concerns about his home.",
+    introduction: 'In today\'s technological and advanced era, we enjoy a multitude of smart devices. Those devices improve the quality of our lives and make them easier, safer, and more comfortable.\n\nOur goal is to enable smart and efficient management for our devices which will provide us controlling tools, and real-time monitoring for everyone, everywhere.',
+    userResearch: 'The first step was to go out and speak to users to recognize their pain points. After interviewing a few users, I also read user reviews of known smart home apps.\n\nThe main issue the users pointed at is that there\'s a specific app for each device\'s company, which leads to having too many apps to manage all smart devices in their homes.\nThere were also significant complaints regarding the user interface and interaction elements - most dashboard designs were complex and overwhelming for the users.',
+    informationArchitecture: 'Based on the insights gained from the research, I defined the architecture of the app. This step helps me to validate my solution before jumping to visual wireframes.',
+    phoneDesign: 'The final step was to design user interfaces for mobile and tablet apps. I aimed for a clean design that will focus the user on the main highlights, so I used white-on-white cards and accents them with teal color.\n\n Routines are running automatically, and updates will be trigger push notifications to help the user live with a little fewer concerns about his home.',
     colors: 'Teal combines the calming properties of blue with the renewal qualities of green. It is a revitalizing and rejuvenating color that symbolizes idealism, calmness, communication, clarity of thought, balance, and harmony.',
-    wireframes: "At the point I was satisfied with the architecture, I started to work on the user interface. I started with a low fidelity sketch to understand what works and what doesn’t work, and then jumped and created more detailed and accurate wireframes."
+    wireframes: 'At the point I was satisfied with the architecture, I started to work on the user interface. I started with a low fidelity sketch to understand what works and what doesn’t work, and then jumped and created more detailed and accurate wireframes.'
   };
 
   public userResearchDetails = {
     first: {
       headline: 'Be the one',
-      text: "Users can't manage too many applications - a single app can handle it all."
+      text: 'Users can\'t manage too many applications - a single app can handle it all.'
     },
     second: {
       headline: 'Keep it simple',
@@ -65,6 +68,16 @@ export class WiseHomeComponent {
   ];
 
 
+  public showOffTabletsImages = [
+    '/assets/wise-home/Mobile/Show off tablets/1.png',
+    '/assets/wise-home/Mobile/Show off tablets/2.png',
+    '/assets/wise-home/Mobile/Show off tablets/3.png',
+    '/assets/wise-home/Mobile/Show off tablets/4.png',
+  ];
+
+
+  private readonly isNotDesktopDevice = false;
+
   public increase(x: number): number {
     return x + 1;
   }
@@ -73,14 +86,8 @@ export class WiseHomeComponent {
     e.scrollIntoView({behavior});
   }
 
-
-  private isNotDesktopDevice = false;
-  constructor(private deviceService: DeviceDetectorService, private router: Router) {
-    this.isNotDesktopDevice = !this.deviceService.isDesktop();
-  }
-
   ngOnInit() {
-    if(this.isNotDesktopDevice) {
+    if (this.isNotDesktopDevice) {
       // this.router.navigateByUrl('/mobile-unsupported');
     }
   }
